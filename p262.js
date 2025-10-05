779374305f76f9b103b076e9265575431e08cb84
githubscientist <sathish.datascientist@gmail.com>
2025-10-05 10:55:51 +0530

    262. Find Sum Pair --brute force technique --using function

--- p262.js
+++ p262.js
@@ -11,27 +11,31 @@ inp.on("line", (data) => {
     userInput.push(data);
 });
 
+function isPairExists(numbers, n, k) {
+    // brute force technique
+    // generate all the possible pairs
+    for (let i = 0; i < n - 1; i++) {
+        // for every element at index i
+        for (let j = i + 1; j < n; j++) {
+            if (numbers[i] + numbers[j] === k) {
+                return true;
+            }
+        }
+    }
+
+    // escaped from return true will alone reach here
+    return false;
+}
+
 inp.on("close", () => {
     // userInput = ["7 6", "1 2 3 4 5 6 7"]
     // read the inputs
     const [n, k] = userInput[0].split(' ').map(num => parseInt(num));
     const numbers = userInput[1].split(' ').map(num => parseInt(num));
-    // using flag
-    let foundPair = false;
 
-    // brute force technique
-    // generate all the possible pairs
-    for (let i = 0; i < numbers.length - 1; i++) {
-        // for every element at index i
-        for (let j = i + 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === k) {
-                console.log('yes');
-                foundPair = true;
-                break;
-            }
-        }
-        if (foundPair) break;
+    if (isPairExists(numbers, n, k)) {
+        console.log('yes');
+    } else {
+        console.log('no');
     }
-
-    if (!foundPair) console.log('no');
 });
